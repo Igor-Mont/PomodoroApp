1b44f12eaac3121b8da7244ddb91a460095ac999
Igor Monteiro <igornathandev@gmail.com>
2021-08-23 07:58:30 -0300

    Add libs

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStyle from './styles/global';
+
+function App() {
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
